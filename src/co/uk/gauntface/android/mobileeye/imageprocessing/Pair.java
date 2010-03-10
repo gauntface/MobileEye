9c4628ae45d313a68389eb8f7fd46e0f25356898
Matthew Gaunt <matt@matt-desktop.(none)>
2010-03-10 18:11:07 +0000

    End of day commit 10-March-2010

---
+++ src/co/uk/gauntface/android/mobileeye/imageprocessing/Pair.java
@@ -0,0 +1,23 @@
+package co.uk.gauntface.android.mobileeye.imageprocessing;
+
+public class Pair
+{
+	private Object mArg1;
+	private Object mArg2;
+	
+	public Pair(Object arg1, Object arg2)
+	{
+		mArg1 = arg1;
+		mArg2 = arg2;
+	}
+	
+	public Object getArg1()
+	{
+		return mArg1;
+	}
+	
+	public Object getArg2()
+	{
+		return mArg2;
+	}
+}
--- src/co/uk/gauntface/android/mobileeye/imageprocessing/QuickSegment.java
+++ src/co/uk/gauntface/android/mobileeye/imageprocessing/QuickSegment.java
@@ -8,9 +8,11 @@ import co.uk.gauntface.android.mobileeye.Singleton;
 
 public class QuickSegment
 {
-	private int MAX_PIXEL_VALUE = 256;
+	private int MAX_PIXEL_VALUE = 255;
+	private int REDUCE_AVG_WINDOW_HALF = 5;
+	
 	private int HISTOGRAM_SPACING = 10;
-	private int BUCKET_THRESHOLD = 10;
+	private int BUCKET_MIN_SIZE_THRESHOLD = 50;
 	
 	private int GROUPING_VARIATION = 40;
 	
@@ -26,21 +28,17 @@ public class QuickSegment
 	
 	public int[] segmentImage(int[] pixels, boolean logHistogram)
 	{
-		int[] pixelBucket = new int[MAX_PIXEL_VALUE];
+		int[] pixelBucket = new int[(MAX_PIXEL_VALUE+1)];
 		
 		// Bucket Sort the Pixels [Histogram Essentially]
-		int maxIndex = 0;
-		
 		for(int i = 0; i < pixels.length; i++)
 		{
 			pixelBucket[pixels[i]] = pixelBucket[pixels[i]] + 1;
-			
-			if(pixelBucket[pixels[i]] > pixelBucket[maxIndex])
-			{
-				maxIndex = pixels[i];
-			}
 		}
 		
+		/**
+		 * Output the histogram to SD Card if neccessary
+		 */
 		if(logHistogram == true)
 		{
 			String histogramOuput = new String();;
@@ -48,21 +46,94 @@ public class QuickSegment
 			for(int i = 0; i < pixelBucket.length; i++)
 			{
 				histogramOuput = histogramOuput + i +","+pixelBucket[i]+"\n";
-				//Log.v(Singleton.TAG, "HistogramLog: " + i + " - " + pixelBucket[i]);
 			}
 			
 			Utility.saveTextToSDCard(histogramOuput, "hist.txt");
 		}
 		
-		int[] groups = hillClimb(pixelBucket, maxIndex);
+		/**
+		 * Average and Reduce the data
+		 */
+		int[] reducedPixelBucket = new int[(int)Math.floor(pixelBucket.length / REDUCE_AVG_WINDOW_HALF)];
+		int counter = 0;
+		int maxIndex = 0;
+		for(int i = REDUCE_AVG_WINDOW_HALF; i < MAX_PIXEL_VALUE; i = i + REDUCE_AVG_WINDOW_HALF)
+		{
+			double cumulativeValue = 0;
+			for(int j = i-REDUCE_AVG_WINDOW_HALF; j < i+REDUCE_AVG_WINDOW_HALF; j++)
+			{
+				if(j >= 0 && j < pixelBucket.length)
+				{
+					cumulativeValue = cumulativeValue + pixelBucket[j];
+				}
+				else
+				{
+					throw new RuntimeException("QuickSegment shouldn't be reaching out of bounds of pixel bucket");
+				}
+			}
+			cumulativeValue = cumulativeValue / (REDUCE_AVG_WINDOW_HALF * 2);
+			reducedPixelBucket[counter] = (int) cumulativeValue;
+			
+			if(cumulativeValue > maxIndex)
+			{
+				maxIndex = counter;
+			}
+			
+			counter = counter + 1;
+		}
+		
+		/**
+		 * Output the reduced histogram to SD Card if neccessary
+		 */
+		if(logHistogram == true)
+		{
+			String histogramOuput = new String();;
+			
+			for(int i = 0; i < reducedPixelBucket.length; i++)
+			{
+				histogramOuput = histogramOuput + i +","+reducedPixelBucket[i]+"\n";
+			}
+			
+			Utility.saveTextToSDCard(histogramOuput, "redhist.txt");
+		}
+		
+		int[] groups = hillClimb(reducedPixelBucket, maxIndex);
+		
+		/**
+		 * Output the peaks / groups to SD Card if neccessary
+		 */
+		if(logHistogram == true)
+		{
+			String histogramOuput = new String();;
+			
+			for(int i = 0; i < groups.length; i++)
+			{
+				histogramOuput = histogramOuput + i +","+groups[i]+"\n";
+			}
+			
+			Utility.saveTextToSDCard(histogramOuput, "groups.txt");
+		}
 		
 		int[] newPixels = new int[pixels.length];
 		
+		Pair[] groupValues = new Pair[groups.length];
+		for(int i = (groups.length - 1); i > 0; i--)
+		{
+			// TODO Make this span more of the histogram lowest 1, next one up 2, third one third
+			int minGroupValue = (groups[i] - 1) * REDUCE_AVG_WINDOW_HALF;
+			int maxGroupValue = ((groups[i] + 1) * REDUCE_AVG_WINDOW_HALF) + (2 * REDUCE_AVG_WINDOW_HALF);
+			
+			groupValues[i] = new Pair(minGroupValue, maxGroupValue);
+		}
+		
 		for(int i = 0; i < pixels.length; i++)
 		{
-			for(int j = 0; j < groups.length; j++)
+			for(int j = 0; j < groupValues.length; j++)
 			{
-				if(pixels[i] >= (groups[j] - GROUPING_VARIATION) && pixels[i] <= (groups[j] + GROUPING_VARIATION))
+				Pair groupValue = groupValues[j];
+				//if(pixels[i] >= (groups[j] - GROUPING_VARIATION) && pixels[i] <= (groups[j] + GROUPING_VARIATION))
+				//{
+				if(pixels[i] >= ((Number)groupValue.getArg1()).intValue() && pixels[i] < ((Number)groupValue.getArg2()).intValue())
 				{
 					if(j == 0)
 					{
@@ -91,11 +162,12 @@ public class QuickSegment
 		return newPixels;
 	}
 	
-	private int[] hillClimb(int[] pixelBucket, int maxIndex)
+	private int[] hillClimb(int[] data, int maxIndex)
 	{
 		ArrayList<Integer> maxPoints = new ArrayList<Integer>();
 		
-		int[] hillStartPoints = new int[]{0, 50, 100, 150, 200, 250, maxIndex};
+		// Make this dynamic
+		int[] hillStartPoints = new int[]{0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, maxIndex};
 		
 		for(int i = 0; i < hillStartPoints.length; i++)
 		{
@@ -103,17 +175,26 @@ public class QuickSegment
 			boolean maximumFound = false;
 			while(maximumFound == false)
 			{
-				if((currentPoint + 1) < MAX_PIXEL_VALUE && pixelBucket[currentPoint + 1] > pixelBucket[currentPoint])
+				if((currentPoint + 1) < data.length && data[currentPoint + 1] > data[currentPoint])
 				{
 					currentPoint = currentPoint + 1;
 				}
-				else if((currentPoint - 1) >= 0 && pixelBucket[currentPoint - 1] > pixelBucket[currentPoint])
+				/**else if((currentPoint + 2) < data.length && data[currentPoint + 2] > data[currentPoint])
+				{
+					currentPoint = currentPoint + 2;
+				}**/
+				else if((currentPoint - 1) >= 0 && data[currentPoint - 1] > data[currentPoint])
 				{
 					currentPoint = currentPoint - 1;
 				}
+				/**else if((currentPoint - 2) >= 0 && data[currentPoint - 2] > data[currentPoint])
+				{
+					currentPoint = currentPoint - 2;
+				}**/
 				else
 				{
-					if(pixelBucket[currentPoint] > BUCKET_THRESHOLD)
+					// Make sure small values don't get considered as a group
+					if(data[currentPoint] > BUCKET_MIN_SIZE_THRESHOLD)
 					{
 						maxPoints.add(currentPoint);
 					}
@@ -130,25 +211,20 @@ public class QuickSegment
 		{
 			int maxPoint = maxPoints.get(i);
 			
-			if(!((maxPoint < (group1 + HISTOGRAM_SPACING)
-					&& maxPoint > (group1 - HISTOGRAM_SPACING))
-				|| (maxPoint < (group2 + HISTOGRAM_SPACING)
-						&& maxPoint > (group2 - HISTOGRAM_SPACING))
-				|| (maxPoint < (group3 + HISTOGRAM_SPACING)
-						&& maxPoint > (group3 - HISTOGRAM_SPACING))))
+			if(maxPoint != group1 && maxPoint != group2 && maxPoint != group3)
 			{
-				if(maxPoint > group1)
+				if(group1 == -1 || data[maxPoint] > data[group1])
 				{
 					group3 = group2;
 					group2 = group1;
 					group1 = maxPoint;
 				}
-				else if(maxPoint > group2)
+				else if(group2 == -1 || data[maxPoint] > data[group2])
 				{
 					group3 = group2;
 					group2 = maxPoint;
 				}
-				else if(maxPoint > group3)
+				else if(group3 == -1 || data[maxPoint] > data[group3])
 				{
 					group3 = maxPoint;
 				}
